67d1725a41a144cddd4a549cbe15aef4a10db4e5
大場育雄 <ohba.ikuo@lxiv.co.jp>
2020-04-19 14:53:36 +0900

    MSのQuickStartをPrism版に移植

---
+++ PrismXamarin/QuickStartToPrism/01_SinglePageApplication/Notes/Notes/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Commands;
+using Prism.Navigation;
+using System;
+using System.IO;
+
+namespace Notes.ViewModels
+{
+	public class MainPageViewModel : ViewModelBase
+	{
+		string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
+		string _text;
+		public string Text
+		{
+			get { return _text; }
+			set { SetProperty(ref _text, value); }
+		}
+
+		public DelegateCommand SaveCommand { get; set; }
+		public DelegateCommand DeleteCommand { get; set; }
+		public MainPageViewModel(INavigationService navigationService)
+			: base(navigationService)
+		{
+			Title = "Main Page";
+			SaveCommand = new DelegateCommand(Save);
+			DeleteCommand = new DelegateCommand(Delete);
+
+			if (File.Exists(_fileName))
+			{
+				Text = File.ReadAllText(_fileName);
+			}
+		}
+
+		private void Save()
+		{
+			File.WriteAllText(_fileName, Text);
+		}
+
+		private void Delete()
+		{
+			if(File.Exists(_fileName))
+			{
+				File.Delete(_fileName);
+			}
+			Text = string.Empty;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# サンプルソース置き場
+
+C#+MVVM中心のサンプルが置かれています。
+
+## PrismXamarin
+
+Xamarin.Form + Prismを使用したサンプルが格納されています。MicrosoftのXamarin.FormsのクイックスタートをPrismに移植したサンプルも置いてあります。
